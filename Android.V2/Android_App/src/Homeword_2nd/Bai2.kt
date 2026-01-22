b626fd322c006f0591ee4dc07e253a5cca03cc4c
DShine <Xuanthinhtran.05072005@gmail.com>
2026-01-22 13:32:24 +0700

    full bai tap thuc hanh

---
+++ Android.V2/Android_App/src/Homeword_2nd/Bai2.kt
@@ -0,0 +1,38 @@
+package Homeword_2nd
+fun bai1(){
+    var name: String? = "An"
+    val doDai= name?.length
+    println(doDai)
+}
+fun bai2(){
+    var name: String? = "An"
+    val doDai= name!!.length
+    println(doDai)
+}
+fun bai3(){
+    var email: String? = null
+    email=readLine()!!.toString()
+    val doDai= email!!.length
+    println(email)
+    println(doDai)
+}
+fun bai4(){
+    var score:Int= readLine()!!.toInt()
+    val output: String= if( score >= 5)  "Dau" else "Rot"
+    println(output)
+}
+fun bai5(){
+    var phone: String? = "01234566789"
+    val doDai= phone!!.length
+    val output: String= if( doDai >= 10 )  "So hop le" else "So khong hop le"
+    println(output)
+
+}
+fun main(){
+    bai1()
+    bai2()
+    bai3()
+    bai4()
+    bai5()
+}
+
