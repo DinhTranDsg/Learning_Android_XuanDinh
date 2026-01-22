2336d79fd7348542e72ba54d6c3980af2c923b17
DShine <Xuanthinhtran.05072005@gmail.com>
2026-01-22 13:16:09 +0700

    full bai tap thuc hang

--- Android.V2/Android_App/src/BaiTapIF/Bai2.kt
+++ Android.V2/Android_App/src/BaiTapIF/Bai2.kt
@@ -10,7 +10,29 @@ fun bai2(){
     val doDai= name!!.length
     println(doDai)
 }
+fun bai3(){
+    var email: String? = null
+    email=readLine()!!.toString()
+    val doDai= email!!.length
+    println(email)
+    println(doDai)
+}
+fun bai4(){
+    var score:Int= readLine()!!.toInt()
+    val output: String= if( score >= 5)  "Dau" else "Rot"
+    println(output)
+}
+fun bai5(){
+    var phone: String? = "01234566789"
+    val doDai= phone!!.length
+    val output: String= if( doDai >= 10 )  "So hop le" else "So khong hop le"
+    println(output)
+
+}
 fun main(){
      bai1()
      bai2()
+     bai3()
+    bai4()
+    bai5()
 }
